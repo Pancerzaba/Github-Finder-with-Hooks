420896e4afe3aa2b365838792db89dbb421893ae
Pancerzaba <marta.rozmyslowicz21@gmail.com>
2020-08-13 20:25:47 +0200

    Alert Context Workflow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/useres/Users';
@@ -8,37 +8,27 @@ import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 
 import GithubState from './context/github/githubState';
+import AlertState from './context/alert/AlertState';
 
 import './App.css';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-
-
- 
-
-  //Set Alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-
-    setTimeout(() => setAlert(null), 5000);
-  };
-
+  
   return (
     <GithubState>
+      <AlertState>
       <Router>
         <div className="App">
           <Navbar />
           <div className="container">
-            <Alert alert={alert} />
+            <Alert />
             <Switch>
               <Route
                 exact
                 path="/"
                 render={(props) => (
                   <Fragment>
-                    <Search setAlert={showAlert} />
+                    <Search/>
                     <Users />
                   </Fragment>
                 )}
@@ -49,6 +39,7 @@ const App = () => {
           </div>
         </div>
       </Router>
+      </AlertState>
     </GithubState>
   );
 };
